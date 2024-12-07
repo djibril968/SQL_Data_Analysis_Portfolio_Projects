dca620d2384e52843f9ee8e2a761ec09f28c26aa
djibril968 <djibril.lekan@gmail.com>
2024-12-07 12:59:29 +0200

    update_02

--- Onyx/Project_01.sql
+++ Onyx/Project_01.sql
@@ -1,1 +1,18 @@
 USE Project_01;
+
+SELECT * FROM cards_data
+SELECT * FROM transactions_data
+SELECT * FROM mcc_codes
+SELECT * from users_data
+
+
+--PART A CUSTOMER SEGMENTATION ANALYSIS
+
+--customer distribution across groups
+
+SELECT MAX(yearly_income) AS max_year, MIN(yearly_income) AS min_income,
+        MAX(total_debt) AS max_debt, MIN(total_debt) AS min_debt,
+        MAX(credit_score) AS max_cs, MIN(credit_score) AS min_cs
+FROM users_data
+
+
